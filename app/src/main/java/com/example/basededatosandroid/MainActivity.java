389c92a70dc5e979863507139ab6c856146b70cd
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2021-08-21 17:27:34 -0500

    Funcionalidad modificar

--- app/src/main/java/com/example/basededatosandroid/MainActivity.java
+++ app/src/main/java/com/example/basededatosandroid/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.basededatosandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.sql.SQLData;
 
 public class MainActivity extends AppCompatActivity {
     private EditText codigo,descripcion,precio;
@@ -23,10 +23,11 @@ public class MainActivity extends AppCompatActivity {
         precio = findViewById(R.id.txt_precio);
     }
 
-    //btn registrar
+    //======================btn registrar===================================================================================
     //primero debi haber creado la clase AdminSQLiteOpenHelper
     public void registrar(View view){
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion",null,1);
+        //Se abre la base de datos en modo lectura y escritura
         SQLiteDatabase baseDeDatos = admin.getWritableDatabase();
         String codigoString,descripcionString,precioString;
         codigoString = codigo.getText().toString();
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         //empty significa vacio
 
-        if(!codigoString.isEmpty() && descripcionString.isEmpty() && precioString.isEmpty()){
+        if(!codigoString.isEmpty() && !descripcionString.isEmpty() && !precioString.isEmpty()){
             ContentValues registrar = new ContentValues();
             registrar.put("codigo",codigoString);
             registrar.put("descripcion",descripcionString);
@@ -51,8 +52,92 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Toast.makeText(this,"Debes llenar todos los campos",Toast.LENGTH_SHORT).show();
         }
-
-
     }
+    //====================== Fin btn registrar===================================================================================
+    //======================btn Consultar===================================================================================
+    public void buscar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion",null,1);
+        //Se abre la base de datos en modo lectura y escritura
+        SQLiteDatabase baseDeDatos = admin.getWritableDatabase();
+        String codigoStringo= codigo.getText().toString();
+        //empty significa vacio
+
+        if(!codigoStringo.isEmpty() ){
+            String sqlSelect = "Select descripcion,precio from articulos where codigo = " + codigoStringo;
+            Cursor fila = baseDeDatos.rawQuery(sqlSelect,null);
+            //moveToFirst -> Revisa si la consulta contiene valores
+            if(fila.moveToFirst()){
+                descripcion.setText(fila.getString(0));
+                precio.setText(fila.getString(1));
+                baseDeDatos.close();
+            }else{
+                Toast.makeText(this,"No exite el articulo",Toast.LENGTH_SHORT).show();
+                baseDeDatos.close();
+            }
+        }else{
+            Toast.makeText(this,"Debes introducir el codigo del articulo",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //======================Fin btn Consultar===================================================================================
+//====================== btn Eliminar===================================================================================
+public void Eliminar(View view){
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion",null,1);
+    //Se abre la base de datos en modo lectura y escritura
+    SQLiteDatabase baseDeDatos = admin.getWritableDatabase();
+    String codigoString= codigo.getText().toString();
+    if(!codigoString.isEmpty()){
+        int cantidad = baseDeDatos.delete("articulos","codigo=" + codigoString,null);
+        baseDeDatos.close();
+        codigo.setText("");
+        descripcion.setText("");
+        precio.setText("");
+       if (cantidad ==1){
+           Toast.makeText(this,"Articulo eliminado exitosamente",Toast.LENGTH_SHORT).show();
+       }else{
+           Toast.makeText(this,"El articulo no existe",Toast.LENGTH_SHORT).show();
+       }
+    }else{
+        Toast.makeText(this,"Debes introducir el codigo del articulo",Toast.LENGTH_SHORT).show();
+        baseDeDatos.close();
+    }
+}
+//======================Fin btn Eliminar===================================================================================
+//====================== btn Modificar===================================================================================
+public void modificar (View view){
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion",null,1);
+    //Se abre la base de datos en modo lectura y escritura
+    SQLiteDatabase baseDeDatos = admin.getWritableDatabase();
+
+    String codigoString,descripcionString,precioString;
+    codigoString = codigo.getText().toString();
+    descripcionString = descripcion.getText().toString();
+    precioString = precio.getText().toString();
+
+    //empty significa vacio
+    if(!codigoString.isEmpty() && !descripcionString.isEmpty() && !precioString.isEmpty()){
+        ContentValues modificar = new ContentValues();
+        modificar.put("codigo",codigoString);
+        modificar.put("descripcion",descripcionString);
+        modificar.put("precio",precioString);
+
+        //Modificar los registrs en la base de datos
+        int cantidad = baseDeDatos.update("articulos",modificar,"codigo=" + codigoString,null);
+        baseDeDatos.close();
+        if(cantidad == 1){
+            Toast.makeText(this,"Articulo " + descripcionString + " modificado exitosamente",Toast.LENGTH_SHORT ).show();
+        }else{
+            Toast.makeText(this,"El articulo no existe",Toast.LENGTH_SHORT).show();
+        }
+        codigo.setText("");
+        descripcion.setText("");
+        precio.setText("");
+        Toast.makeText(this,"Registro Exitoso del producto codigo: " + codigoString,Toast.LENGTH_SHORT ).show();
+    }else{
+        Toast.makeText(this,"Debes llenar todos los campos",Toast.LENGTH_SHORT).show();
+    }
+
+}
+//======================Fin btn Modificar===================================================================================
 
 }
